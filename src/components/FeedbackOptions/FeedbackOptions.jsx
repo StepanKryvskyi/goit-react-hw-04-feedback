ce371a0cbb43543baca09c534ae1a28513773bb7
StepanKryvskyi <126700161+StepanKryvskyi@users.noreply.github.com>
2023-10-11 21:13:50 +0300

    Initial commit

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,15 @@
+import css from './FeedbackOptions.module.css'
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul className={css.btnList}>
+      {options.map((option, index) => (
+        <li key={index}>
+          <button className={css.feedbackBtb} id={option} onClick={e => onLeaveFeedback(e)}>
+            {option}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
